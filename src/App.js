380f050391b55ebd24a4bf5ccc9d2a8247dac6d3
chuongvu2001 <chuongvu2806@gmail.com>
2021-03-26 23:21:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap'
+import {BrowserRouter,Route} from 'react-router-dom'
+import Header from './Header'
+import Login from './Login'
+import Register from './Register'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
+
+
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Header></Header>
+     
      <h1>E-Comm Project</h1>
-     <button>Normal Button</button>
-     <Button>Bootstrap Button</Button>
+    <Route path="/login">
+      <Login></Login>
+    </Route>
+    <Route path="/register">
+      <Register></Register>
+    </Route>
+    <Route path="/add">
+      <AddProduct></AddProduct>
+    </Route>
+    <Route path="/update">
+      <UpdateProduct></UpdateProduct>
+    </Route>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+function Header() {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto nav_bar_wrapper">
+                    <Link to="/add">Add Product</Link>
+                    <Link to="/update">Update Product</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
+export default Header
